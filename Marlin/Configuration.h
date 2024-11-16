2b370f085d9eead8b13a27355242c6ec72886cd4
Jan Dietrich <jan.dietrich@codethis.one>
2024-11-17 00:43:13 +0100

    probe offset changed to new bltouch mount

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1518,7 +1518,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -30, -42, -3 }
+#define NOZZLE_TO_PROBE_OFFSET { 38, -47, -1.2 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
